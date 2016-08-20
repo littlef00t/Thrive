4ed4e91525b0eef0968c7a0a118e742c80af3bc1
Christine Liao <astridcliao@gmail.com>
2016-08-19 22:28:50 -0700

    added shelters table, migrated

--- db/migrate/20160820050755_create_shelters.rb
+++ db/migrate/20160820050755_create_shelters.rb
@@ -1,6 +1,10 @@
 class CreateShelters < ActiveRecord::Migration
   def change
     create_table :shelters do |t|
+      t.string :name, null: false
+      t.text :description
+      t.float :long, null: false
+      t.float :lat, null: false
 
       t.timestamps null: false
     end
