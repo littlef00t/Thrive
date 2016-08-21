efccdbae73d3cdea81e8f8cd0f38dc1b4c57cc8c
Christine Liao <astridcliao@gmail.com>
2016-08-21 16:26:57 -0700

    root staticpages to static_pages following file format

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :shelters, only: [:index, :show, :create]
   end
 
-  root "staticpages#root"
+  root "static_pages#root"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
