832709d7a604d258191a1fcf4a67974ac7ec0b2b
Christine Liao <astridcliao@gmail.com>
2016-08-22 18:13:06 -0700

    added store and tested

--- frontend/thrive.jsx
+++ frontend/thrive.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.Store = configureStore();
   ReactDOM.render(<div>Breathe</div>, document.getElementById('content'));
 });
