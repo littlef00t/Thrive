c3c620487be8f9fc3ddb8bb54b39f34bb10b8a81
Christine Liao <astridcliao@gmail.com>
2016-08-23 19:27:18 -0700

    added rootmiddleware to store, tested

--- frontend/thrive.jsx
+++ frontend/thrive.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import { requestShelters, receiveShelters } from './actions/shelters_actions';
+
+window.requestShelters = requestShelters;
+window.receiveShelters = receiveShelters;
 
 
 document.addEventListener('DOMContentLoaded', () => {
