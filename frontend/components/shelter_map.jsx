2b490dc8dfed7e121481979daab246bf888ca32f
Christine Liao <astridcliao@gmail.com>
2016-09-09 21:02:01 -0700

    map showing

--- frontend/components/shelter_map.jsx
+++ frontend/components/shelter_map.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class ShelterMap extends React.Component{
+  componentDidMount(){
+    // find the `<map>` node on the DOM
+    const mapDOMNode = this.refs.map;
+
+    // set the map to show SF
+    const mapOptions = {
+      center: {lat: 37.7758, lng: -122.435}, // this is SF
+      zoom: 13
+    };
+
+    // wrap the mapDOMNode in a Google Map
+    this.map = new google.maps.Map(mapDOMNode, mapOptions);
+  }
+
   render(){
 
-    return <div id='map-container' ref='map'>
-      I am a map!
-    </div>
+    return (
+      <div>
+        <div id='map-container' ref='map'></div>
+      </div>
+    )
   }
 }
 
